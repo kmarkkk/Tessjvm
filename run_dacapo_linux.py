d953f4ce8f301b3ce922d18ca69905f27c1a9f36
Benjamin Le <BenjaminHoanLe@gmail.com>
2014-11-12 14:21:43 -0800

    run_dacapo_linux now clean itself up on failure and save system state when running the experiement

--- run_dacapo_linux.py
+++ run_dacapo_linux.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import argparse
+import datetime
+import json
 import os
 import shutil
 import subprocess
@@ -20,15 +22,26 @@ def mkdir(directory, clean=False):
         shutil.rmtree(directory)
         os.makedirs(directory)
 
-def runDacapo(options):
+def cleanUp(options, procs_and_files):
+    #Cleanup Scratch Directories
+    for i in range(options.numjvms):
+        subprocess.Popen(['rm', '-rf', 'scratch%d' % i])
 
+    #Kill Ongoing processes
+    if procs_and_files:
+        for proc, stdout in procs_and_files:
+            proc.kill()
+            stdout.close()
+
+def runDacapo(options):
+    # Build the Directory Structure
     resultsdir = options.resultsdir
     experimentdir = os.path.join(resultsdir, EXPERIMENT)
     typedir = os.path.join(experimentdir, TYPE)
 
     mkdir(resultsdir)
     mkdir(experimentdir)
-    mkdir(typedir, clean=True)
+    mkdir(typedir)
 
     # Parse which dacapo benchmark to run
     if options.benchmark == "all":
@@ -37,6 +50,16 @@ def runDacapo(options):
         assert options.benchmark in ALL_BENCHMARKS
         benchmarks = [options.benchmark]
 
+    # Save experiement system state (revision #, env vars, timestamp, benchmark(s) run)
+    sys_state = dict()
+    sys_state['git_revision'] = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+    sys_state['env_vars'] = dict(os.environ)
+    sys_state['benchmarks'] = benchmarks
+    sys_state_file = open(os.path.join(typedir, 'sys_state_%s.json' % datetime.datetime.now().isoformat()), 'w')
+    json.dump(sys_state, sys_state_file, sort_keys=True, indent=4, separators=(',', ': '))
+    sys_state_file.close()
+
+    # Run Benchmarks under various numbers of JVMS and Heap Sizes
     for benchmark in benchmarks:
         printVerbose(options, "Benchmark: %s" % benchmark)
         numjvms = 1
@@ -44,27 +67,32 @@ def runDacapo(options):
             printVerbose(options, "Num JVMs: %d" % numjvms)
             heapsize = 128
             while heapsize <= options.maxheap:
-                printVerbose(options, "Heapsize: %dMB" % heapsize)
-                procs = []
-                for i in range(numjvms):
-                    outputdir = os.path.join(typedir, "%s_%djvms_%dMB" % (benchmark, numjvms, heapsize))
-                    mkdir(outputdir)
+                try:
+                    printVerbose(options, "Heapsize: %dMB" % heapsize)
+                    procs_and_files = []
+                    for i in range(numjvms):
+                        outputdir = os.path.join(typedir, "%s_%djvms_%dMB" % (benchmark, numjvms, heapsize))
+                        mkdir(outputdir, clean=True)
 
-                    stdout = open(os.path.join(outputdir, 'stdout'), 'a')
+                        stdout = open(os.path.join(outputdir, 'stdout'), 'a')
 
-                    cmd = ['java', '-Xmx%dM' % heapsize, '-jar', options.dacapo, '--scratch-directory', 'scratch%d' % i, benchmark]
-                    printVerbose(options, " ".join(cmd))
-                    proc = subprocess.Popen(cmd, stdout=stdout, stderr=stdout)
-                    procs.append(proc)
+                        cmd = ['java', '-Xmx%dM' % heapsize, '-jar', options.dacapo, '--scratch-directory', 'scratch%d' % i, benchmark]
+                        printVerbose(options, " ".join(cmd))
+                        proc = subprocess.Popen(cmd, stdout=stdout, stderr=stdout)
+                        procs_and_files.append((proc, stdout))
 
-                for proc in procs:
-                    proc.wait()
-                heapsize *= 2
+                    for proc, stdout in procs_and_files:
+                        proc.wait()
+                        stdout.close()
+                        procs_and_files.remove((proc, stdout))
+                    heapsize *= 2
+                except KeyboardInterrupt as e:
+                    print "Detecting KeyboardInterrupt: Cleaning up Experiements"
+                    cleanUp(options, procs_and_files)
+                    raise e
             numjvms *= 2
 
-    #Cleanup Scratch Directories
-    for i in range(options.numjvms):
-        subprocess.Popen(['rm', '-rf', 'scratch%d' % i])
+    cleanUp(options, procs_and_files)
 
 
 if __name__ == "__main__":
