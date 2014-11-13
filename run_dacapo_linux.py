60258b3db8359ff328ab77d855aa711290adc5fa
Benjamin Le <BenjaminHoanLe@gmail.com>
2014-11-12 16:09:53 -0800

    Save CPU/Mem in sys state and fixed bug where stdout was being closed too early

--- run_dacapo_linux.py
+++ run_dacapo_linux.py
@@ -22,16 +22,43 @@ def mkdir(directory, clean=False):
         shutil.rmtree(directory)
         os.makedirs(directory)
 
-def cleanUp(options, procs_and_files):
+def cleanUp(options, procs, stdout):
     #Cleanup Scratch Directories
     for i in range(options.numjvms):
         subprocess.Popen(['rm', '-rf', 'scratch%d' % i])
 
     #Kill Ongoing processes
-    if procs_and_files:
-        for proc, stdout in procs_and_files:
+    if procs:
+        for proc in procs:
             proc.kill()
             stdout.close()
+
+    #Flush and Close the currently open stdout file
+    if stdout:
+        stdout.close()
+
+def parseCpuModel():
+    #Adapted from http://amitsaha.github.io/site/notes/articles/python_linux/article.html
+    try:
+        with open('/proc/cpuinfo') as f:
+            for line in f:
+                if line.strip() and line.rstrip('\n').startswith('model name'):
+                    model_name = line.rstrip('\n').split(':')[1]
+                    return model_name
+    except IOError:
+        return "Unknown"
+
+def parseMemory():
+    #Adapted from http://amitsaha.github.io/site/notes/articles/python_linux/article.html
+    try:
+        with open('/proc/meminfo') as f:
+            for line in f:
+                key, value = line.split(':')
+                if key == 'MemTotal':
+                    return value 
+    except IOError:
+        return "Unknown"
+    return "Unknown"
 
 def runDacapo(options):
     # Build the Directory Structure
@@ -55,6 +82,8 @@ def runDacapo(options):
     sys_state['git_revision'] = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
     sys_state['env_vars'] = dict(os.environ)
     sys_state['benchmarks'] = benchmarks
+    sys_state['CPU'] = parseCpuModel()
+    sys_state['Memory'] = parseMemory()
     sys_state_file = open(os.path.join(typedir, 'sys_state_%s.json' % datetime.datetime.now().isoformat()), 'w')
     json.dump(sys_state, sys_state_file, sort_keys=True, indent=4, separators=(',', ': '))
     sys_state_file.close()
@@ -69,30 +98,32 @@ def runDacapo(options):
             while heapsize <= options.maxheap:
                 try:
                     printVerbose(options, "Heapsize: %dMB" % heapsize)
-                    procs_and_files = []
+                    procs = []
+
+                    outputdir = os.path.join(typedir, "%s_%djvms_%dMB" % (benchmark, numjvms, heapsize))
+                    mkdir(outputdir, clean=True)
+                    stdout = open(os.path.join(outputdir, 'stdout'), 'a')
                     for i in range(numjvms):
-                        outputdir = os.path.join(typedir, "%s_%djvms_%dMB" % (benchmark, numjvms, heapsize))
-                        mkdir(outputdir, clean=True)
-
-                        stdout = open(os.path.join(outputdir, 'stdout'), 'a')
-
                         cmd = ['java', '-Xmx%dM' % heapsize, '-jar', options.dacapo, '--scratch-directory', 'scratch%d' % i, benchmark]
                         printVerbose(options, " ".join(cmd))
-                        proc = subprocess.Popen(cmd, stdout=stdout, stderr=stdout)
-                        procs_and_files.append((proc, stdout))
+                        if options.stdout:
+                            proc = subprocess.Popen(cmd)
+                        else:
+                            proc = subprocess.Popen(cmd, stdout=stdout, stderr=stdout)
+                        procs.append(proc)
 
-                    for proc, stdout in procs_and_files:
+                    for proc in procs:
                         proc.wait()
-                        stdout.close()
-                        procs_and_files.remove((proc, stdout))
+                        procs.remove(proc)
+                    stdout.close()
                     heapsize *= 2
                 except KeyboardInterrupt as e:
                     print "Detecting KeyboardInterrupt: Cleaning up Experiements"
-                    cleanUp(options, procs_and_files)
+                    cleanUp(options, procs, stdout)
                     raise e
             numjvms *= 2
 
-    cleanUp(options, procs_and_files)
+    cleanUp(options, procs, stdout)
 
 
 if __name__ == "__main__":
@@ -104,6 +135,7 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--resultsdir", action="store", help="where to store results")
     parser.add_argument("-m", "--maxheap", action="store", type=int, default=4096, help="max head size")
     parser.add_argument("-v", "--verbose", action="store_true", default=False, help="be more verbose")
+    parser.add_argument("-s", "--stdout", action="store_true", default=False, help="Output to stdout rather than to results dir")
     cmdargs = parser.parse_args()
     
     runDacapo(cmdargs)
