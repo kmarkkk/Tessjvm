26e62f3ae14c6823ae547fb086044c03f3703663
Benjamin Le <BenjaminHoanLe@gmail.com>
2014-11-14 01:58:00 -0800

    Change format of dacapo command passed to OSv

--- run_dacapo.py
+++ run_dacapo.py
@@ -116,7 +116,7 @@ def runDacapo(options):
                         cmd = ['java', '-Xmx%dM' % heapsize, '-jar', options.dacapo, '--scratch-directory', 'scratch%d' % i, benchmark]
 
                         if options.xen:
-                            dacapo_cmd = " ".join(cmd)
+                            dacapo_cmd = " ".join(['/java.so', '-Xmx%dM' % heapsize, '-jar', "/dacapo.jar", benchmark])
                             cmd = ["./scripts/run.py", "-i", options.image, "-m", options.memsize, "-c", options.vcpus, 
                                     '-e', "'%s'" % dacapo_cmd, '-p xen']
 
