64eeef57084b8650592886637df22ee542169950
Benjamin Le <BenjaminHoanle@gmail.com>
2014-11-15 16:42:37 -0800

    Strip the output to get rid of the new line

--- scripts/run.py
+++ scripts/run.py
@@ -239,7 +239,7 @@ def start_osv_xen(options):
     ]
 
     if options.losetup:
-    	loopdev = subprocess.Popen(['sudo', 'losetup', '-f'], stdout=subprocess.PIPE).communicate()[0]
+    	loopdev = subprocess.Popen(['sudo', 'losetup', '-f'], stdout=subprocess.PIPE).communicate()[0].strip()
     	args += ["disk=['%s,raw,hda,rw']" % loopdev]
     else:
     	args += ["disk=['%s,qcow2,hda,rw']" % options.image_file]
