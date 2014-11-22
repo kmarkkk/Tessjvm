80e04b6a916d4d1f106e86d2e99fcfc68163f674
Jefferson Lai <jefflai2@gmail.com>
2014-11-22 00:04:06 -0800

    Cleaned up a little and compute mean

--- parse_dacapo.py
+++ parse_dacapo.py
@@ -10,20 +10,22 @@ DACAPO_DIR='dacapo'
 DACAPO_BENCHMARKS = ['avrora', 'h2', 'jython', 'luindex', 'lusearch', 'xalan']
 
 def parse_benchmark(benchmark, benchmark_experiments, os_type):
+  print
+  print "#"*50
+  print "Parsing runtime results for %d %s experiments" % (len(benchmark), benchmark)
+  print "#"*50
+  print
 
-  print "Parsing runtime statistics for %d %s experiments" % (len(benchmark), benchmark)
   runtime_results = parse_runtime_results(benchmark, benchmark_experiments)
 
   print runtime_results
-  #print "Parsing Experiment %s with %d JVMs and memory size = %d" % (benchmark, int(num_jvms), int(mem_size))
   xs, ys, zs = [list(tuple_vector) for tuple_vector in zip(*sorted(runtime_results, key=lambda t: -t[0]))]
   
 
-  ax = plt.subplot(111)
-  ax.bar([x - 0.1 for x in xs], ys, width=0.2, color="b", align="center", log=True)
-  ax.bar([x + 0.1 for x in xs], zs, width=0.2, color="g", align="center", log=True)
-  plt.show()
-  #plot_line(jvm_vec, runtime_vec, "Numeric JVM Identifier", "Runtime in ms", "Dacapo %s on %s" % (benchmark, operating_system))
+  #ax = plt.subplot(111)
+  #ax.bar([x - 0.1 for x in xs], ys, width=0.2, color="b", align="center", log=True)
+  #ax.bar([x + 0.1 for x in xs], zs, width=0.2, color="g", align="center", log=True)
+  #plt.show()
   
   return
 
@@ -44,8 +46,11 @@ def parse_runtime_results(benchmark, benchmark_experiments):
       filename = "/".join([exp_path, "stderr%d" % jvm])
       with open(filename, 'r') as f:
         contents = f.read()
-        m = re.search("PASSED in (\d+) msec", contents)
-        cur_times.append(int(m.groups()[0]))
+        all_times = map(int, re.findall(".*in (\d+) msec", contents))
+        # We use the last five runs 
+        run_times = all_times[-5:]
+        mean_time = float(sum(run_times))/len(all_times)
+        cur_times.append(mean_time)
 
     results.append((num_jvms, mem_size, float(sum(cur_times))/len(cur_times)))
   return results
@@ -71,7 +76,7 @@ if __name__ == "__main__":
   experiments_dir = '/'.join([RESULTS_DIR, DACAPO_DIR, os_type])
   all_experiments = os.listdir(experiments_dir)
 
-  for benchmark in DACAPO_BENCHMARKS[:1]:
+  for benchmark in DACAPO_BENCHMARKS:
     benchmark_experiments = filter(lambda s: re.match("^%s_.*" % benchmark, s), all_experiments)
     parse_benchmark(benchmark, benchmark_experiments, os_type)
 
