875421f8b8f81aacfaf4b43014f11cde9350550d
Jefferson Lai <jefflai2@gmail.com>
2014-12-10 13:49:30 -0800

    updated ycsb

--- parse_ycsb.py
+++ parse_ycsb.py
@@ -8,26 +8,26 @@ import argparse
 from collections import defaultdict, namedtuple
 
 YCSB_DIR = 'cassandra_ycsb'
-RESULT_METRICS = ['iter', 'ovr_runtime', 'ovr_thruput', 'r_latency', 'u_latency']
+RESULT_METRICS = ['ovr_runtime', 'ovr_thruput', 'r_latency', 'u_latency']
 RESULT_LABELS = {'ovr_runtime': ('Average Overall Runtime', 'Time (ms)'),
                  'ovr_thruput': ('Average Overall Throughput', 'Operations per second'),
                  'r_latency': ('Read Operation Latency', u'Latency (\u03bcs)'),
                  'u_latency': ('Update Operation Latency', u'Latency \u03bcs)')}
 
-YCSB_IterResult = namedtuple('YCSB_IterResult', RESULT_METRICS)
+YCSB_Result = namedtuple('YCSB_Result', RESULT_METRICS)
 
-JVM_COUNTS = [1, 2, 4, 6, 8]
+JVM_COUNTS = [1, 2, 4]
 
 def plot(plot_type, experiments, os_type, results_dir, output_dir, output_extension):
-  results = parse_results(experiments, os_type)
-  avg_runtimes = []
-  
-  for jvm_count in JVM_COUNTS:
-    iter_results = results[jvm_count]
-    avg_runtimes.append(np.mean([getattr(iter_result, plot_type) for iter_result in iter_results]))
+  ycsb_results = parse_results(experiments, os_type)
 
+  metric_values = []  
   plt.clf()
-  plt.plot(JVM_COUNTS, avg_runtimes, '-d')
+  for jvm_count in JVM_COUNTS:
+    ycsb_result = ycsb_results[jvm_count]
+    metric_values.append(getattr(ycsb_result, plot_type))
+  
+  plt.plot(JVM_COUNTS, metric_values, '-d')
   plt.title(RESULT_LABELS[plot_type][0])
   plt.ylabel(RESULT_LABELS[plot_type][1])
   plt.xlabel("Number of JVMs")
@@ -37,28 +37,31 @@ def plot(plot_type, experiments, os_type, results_dir, output_dir, output_extens
 
 def parse_results(experiments, os_type):
   # Returns dictionary of the form: {num_jvms -> YCSB_Result}
-  jvms_to_results = {}
+  jvms_to_results = defaultdict(list)
   for exp in experiments:
     jvm_count = int(re.search("(\d+)jvms$", exp).groups()[0])
     exp_path = "/".join([results_dir, YCSB_DIR, os_type, exp])
-
-    num_iterations = 5
-    results = []
-    for iteration in range(1, num_iterations+1):
-      # Runtimes are logged in the stderr files on linux and stout files on xen
-      if os_type == "xen":
-        filename = "/".join([exp_path, "ycsbrunstdout%02d" % iteration])
-      else:
-        filename = "/".join([exp_path, "ycsbrunstdout%02d" % iteration])
-      with open(filename, 'r') as f:
-        contents = f.read()
-
-      overall_runtime_ms = float(re.search("RunTime\(ms\), (\d+\.\d+)", contents).groups()[0])
-      overall_throughput_ops_sec = float(re.search("Throughput\(ops/sec\), (\d+\.\d+)", contents).groups()[0])
-      update_latency, read_latency, cleanup_latency = map(float, re.findall("AverageLatency\(us\), (\d+\.\d+)", contents))
-      
-      results.append(YCSB_IterResult(iteration, overall_runtime_ms, overall_throughput_ops_sec, update_latency, read_latency))
-    jvms_to_results[jvm_count] = results
+    for jvm in range(1, jvm_count+1):
+      num_iterations = 5
+      results = defaultdict(list)
+      for iteration in range(1, num_iterations+1):
+        # Runtimes are logged in the stderr files on linux and stout files on xen
+        if os_type == "xen":
+          filename = "/".join([exp_path, "ycsbrunstdout%02d%02d" % (jvm, iteration)])
+        else:
+          filename = "/".join([exp_path, "ycsbrunstdout%02d%02d" % (jvm, iteration)])
+        with open(filename, 'r') as f:
+          contents = f.read()
+        # Store results from each iteration this JVM ran
+        results['ovr_runtime'].append(float(re.search("RunTime\(ms\), (\d+\.\d+)", contents).groups()[0]))
+        results['ovr_thruput'].append(float(re.search("Throughput\(ops/sec\), (\d+\.\d+)", contents).groups()[0]))
+        u_latency, r_latency, cleanup_latency = map(float, re.findall("AverageLatency\(us\), (\d+\.\d+)", contents))
+        results['u_latency'].append(u_latency)
+        results['r_latency'].append(r_latency)
+      # Average across iterations
+      averages = [np.mean(results[metric]) for metric in RESULT_METRICS]
+      # Append a single result for each JVM instance
+      jvms_to_results[jvm_count] = YCSB_Result(*averages)
   return jvms_to_results
 
 def save_or_show_current(output_dir, plot_type, output_extension):
@@ -78,7 +81,7 @@ if __name__ == "__main__":
   parser.add_argument("-o", "--outputdir", action="store", default=False, help="provide a directory to save the experiment results into")
   parser.add_argument("-e", "--extension", action="store", default="eps", help="if -o is provided, this is the file type extension for the graph images")
   cmdargs = parser.parse_args()
-  
+
   results_dir = cmdargs.resultsdir
 
   experiments_dir = '/'.join([results_dir, YCSB_DIR, cmdargs.xen])
@@ -86,9 +89,9 @@ if __name__ == "__main__":
 
   experiments = filter(lambda s: re.match(".*jvms$", s), all_experiments)
   if cmdargs.type == 'all':
-    for metric in RESULT_METRICS[1:]:
+    for metric in RESULT_METRICS:
       plot(metric, experiments, cmdargs.xen, results_dir, cmdargs.outputdir, cmdargs.extension)
   else:
     plot(cmdargs.type, experiments, cmdargs.xen, results_dir, cmdargs.outputdir, cmdargs.extension)
-    
+
 
