ec123a626871a28a431c1cd4db5c99b19604f12e
Ben <BenjaminHoanLe@gmail.com>
2014-12-11 23:35:31 -0800

    Do extra 5 iterations instead of fixed 20

--- run_dacapo.py
+++ run_dacapo.py
@@ -200,7 +200,7 @@ def runDacapo(options):
                         for i in range(numjvms):
                             numIterations = 40
                             if options.gangscheduled:
-                                numIterations = 20
+                                numIterations = numBenchmarkIterations + 5
                             dacapo_cmd = " ".join(['/java.so', '-Xmx%dM' % heapsize, '-XX:+PrintGCTimeStamps', '-XX:+PrintGCDetails', '-XX:+UseParallelOldGC',
                                                      '-jar', "/dacapo.jar", "-n", str(numIterations), benchmark])
                             cmd = dacapoXenRunCommand(options, i, numjvms)
